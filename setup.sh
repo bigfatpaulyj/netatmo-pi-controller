ce03bbff9f90655d45a908ebc0b8674ec8dcbd91
paul.white <p4ul.white@gmail.com>
2023-12-02 23:37:49 +0000

    add comment to setup

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ sudo apt-get install python3-pip
 sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo pip3 install Adafruit_DHT
 sudo apt install prometheus
+echo "Dont forget to configure prometheus at /etc/prometheus/prometheus.yml"
